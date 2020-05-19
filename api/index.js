68d83b7a172cc15773b3d24d3d29ae88ed9abcd3
ralmeidax <ricardo.de.almeidax@gmail.com>
2020-05-19 01:57:17 -0300

    create write file method

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const http = require('http')
-//const URL = require('url')
-const content = require('./guias.json')
-//const path = require('path')
+const URL = require('url')
+const guias = require('./guias.json')
+const agendamento = require('./agendamentos.json')
+const path = require('path')
+const fs = require('fs')
 
-const portNumber = 5000
+const portNumberApi1 = 5000
+const portNumberApi2 = 3800
 
 http.createServer((req,res) => {
 
@@ -11,6 +14,48 @@ http.createServer((req,res) => {
         'Access-Control-Allow-Origin': '*'
     })
 
-    return res.end(JSON.stringify(content))
+    return res.end(JSON.stringify(guias))
 
-}).listen(portNumber,() => console.log(`Server Api is Running - Port: ${portNumber}`))
+}).listen(portNumberApi1,() => console.log(`Server Api Guias is Running - Port: ${portNumberApi1}`))
+
+
+http.createServer((req,res) => {
+    const { codigo, nome, foto, dataInicio, dataFim, regiao, del } = URL.parse(req.url,true).query
+    
+    res.writeHead(200,{
+        'Access-Control-Allow-Origin': '*'
+    })
+
+    //All Resources
+    if(!codigo || !nome)
+        return res.end(JSON.stringify(agendamento))
+
+    //Delete
+    if(del){
+        console.log(`delete dedicante: ${codigo}`)
+        agendamento.agendamentos = agendamento.agendamentos.filter(item => String(item.codigo) !== String(codigo))
+        
+        return writeFile((message) => {
+            res.end(message)
+        })
+    }
+
+    //Create
+    agendamento.agendamentos.push({codigo, nome, foto, dataInicio, dataFim, regiao})
+        console.log(`adding dedicante: ${codigo} - ${nome}`)
+        return writeFile((message) => res.end(message))
+
+}).listen(portNumberApi2,() => console.log(`Server Api Agendamento is Running - Port: ${portNumberApi2}`))
+
+
+function writeFile(newPlanning){
+    fs.writeFile(
+        path.join(__dirname,'agendamentos.json'),
+        JSON.stringify(agendamento,null,2),
+        err => {
+            if(err) throw err
+
+            newPlanning(JSON.stringify({message: 'ok'}))
+        }
+    )
+}
--- public/planejando.js
+++ public/planejando.js
@@ -12,16 +12,18 @@ btnAdd.addEventListener('click',addDedicantes)
 
 
 function addDedicantes(){
+    /*
     const dadosDedicante = selDedicantes.value.split('|')
-
     const idDedicante = dadosDedicante[0]
     const nomeDedicante = dadosDedicante[1]
     const fotoDedicante = dadosDedicante[2]
     const regiaoDedicante = dadosDedicante[3]
+    */
 
+    const [codigo, nome, foto, regiao] = selDedicantes.value.split('|')
 
     const existInList = listaDedicantes.filter((item) => {
-        if (item.codigo == idDedicante) {
+        if (item.codigo == codigo) {
             return item
         }
     })
@@ -34,45 +36,71 @@ function addDedicantes(){
     }
     
     //fabricar dedicante
-    const dedicante = fabricarDedicante(idDedicante, nomeDedicante,fotoDedicante,txtStartDate.value,txtEndDate.value,regiaoDedicante)
+    let dataInicio = txtStartDate.value
+    let dataFim = txtEndDate.value
+    
+    //Gravando no arquivo via API
+    apiAddPlanning({codigo,nome,foto,dataInicio,dataFim,regiao})
 
+
+}
+    
+function addPlanning({codigo, nome, foto, dataInicio, dataFim, regiao}){
+    const dedicante = fabricarDedicante(codigo, nome,foto,dataInicio,dataFim,regiao)
+
+    //inserindo no Array de Dedicantes
     listaDedicantes.push(dedicante)
-
     console.log(listaDedicantes)
 
+ 
+
+    //Inserindo na página HTML
     const li = document.createElement('li')
     const imgFoto = new Image(50,50)
     const a = document.createElement('a')
     const span = document.createElement('span')
+    const divParent = document.createElement('div')
+    const divDates = document.createElement('div')
 
-    imgFoto.src = fotoDedicante
-    a.innerHTML = nomeDedicante
+    imgFoto.src = foto
+    a.innerHTML = nome
     span.innerHTML = `\u{1F5D1}`
-    span.id = idDedicante
-    
-    //span.addEventListener('click',deleteDedicante(10))
+    span.id = codigo
 
+    divDates.style = 'font-size: 0.8rem;text-align: right'
+    divDates.innerHTML = `<strong>Início:</strong> ${dataInicio} <br>`
+    divDates.innerHTML += `<strong>Fim:</strong> ${dataFim}`
+
+    divParent.appendChild(divDates) 
+    
     span.onclick = () => deleteDedicante(span)
 
     li.append(imgFoto)
     li.append(a)
+    li.append(divParent)
     li.append(span)
 
     ulGuias.appendChild(li)
 
-
-
-
 }
 
-async function loadAPI(){
+async function loadAPISelect(){
     const res = await fetch('http://localhost:5000').then((content) => content.json())
 
     res.guias.map((item)=>
         addItemInSelect(item))
 }
 
-loadAPI()
+loadAPISelect()
+
+async function loadAPIPlanning() {
+    const res = await fetch('http://localhost:3800').then((content) => content.json())
+
+    res.agendamentos.map((item) =>
+        addPlanning(item))
+}
+
+loadAPIPlanning()
 
 
 //inserir as pessoas no select
@@ -100,10 +128,26 @@ function deleteDedicante(element) {
     
     if(confirm('Você tem certeza que deseja excluir o dedicante?')){
         element.parentNode.remove()
+        
+        let codigo = element.id
+
+        apiDelPlanning({codigo})
 
         listaDedicantes = listaDedicantes.filter((item) => {
             if(!(item.codigo == element.id)) return item
         })
     }
         
+}
+
+async function apiAddPlanning({codigo, nome, foto, dataInicio, dataFim, regiao}){
+    const urlApi = new URL('http://localhost:3800')
+    urlApi.search = new URLSearchParams({codigo, nome, foto, dataInicio, dataFim, regiao }).toString()
+    await fetch(urlApi)
+}
+
+async function apiDelPlanning({codigo,nome, foto, dataInicio, dataFim, regiao}){
+    const urlApi = new URL('http://localhost:3800')
+    urlApi.search = new URLSearchParams({codigo,nome, foto, dataInicio, dataFim, del:1}).toString()
+    await fetch(urlApi)
 }
